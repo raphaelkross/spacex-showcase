582acb6eac2814a0f93d87accdb80c5e05b930a8
Rafael <xiaothemes@gmail.com>
2021-11-16 17:16:27 -0300

    Fetch and render cards

---
+++ components/LaunchCard/index.tsx
@@ -0,0 +1,39 @@
+import styles from './styles.module.css'
+
+const LaunchCard = (props) => {
+  const {
+    mission_name: missionName,
+    rocket,
+    details,
+    links
+  } = props.data;
+
+  const {
+    rocket_name: rocketName
+  } = rocket;
+
+  const {
+    article_link: articleLink,
+    flickr_images: images,
+  } = links;
+
+  const hasImage = images.length > 0 ? true : false;
+
+  const thumbnail = hasImage ? images[0] : null;
+
+  return (
+    <a className={styles.card} href={articleLink}>
+      <div className={styles.thumb} style={{backgroundImage: `url(${thumbnail})`}}></div>
+      <div className={styles.meta}>
+        <h3>{missionName}</h3>
+        <p className={styles.rocket}>{rocketName}</p>
+        {details ?
+          <p className={styles.description}>{details.length > 150 ? details.substring(0, 147) + '...' : details}</p>
+          : null }
+      </div>
+
+    </a>
+  )
+}
+
+export default LaunchCard
